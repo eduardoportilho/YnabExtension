1034d2d24768bd896bc21f48f5809c1ebefa4f16
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 10:41:48 +0200

    Column finder skeleton.

---
+++ src/scripts/ynab/column-finder.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {ColumnInfo} Description of the columns of tabular data.
+ * @property {} name - description.
+ */
+
+module.exports = {
+  /**
+   * Find information about the columns of tabular data.
+   * @param  {array[]} tabularData - Tabular data.
+   * @return {ColumnInfo} Column information.
+   */
+  getColumnInfo: (tabularData) => {
+    //TODO
+    return []
+  }
+}
