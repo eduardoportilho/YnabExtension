c051b0ee4388e7389e904f80010d6031136bf828
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 08:19:29 +0200

    Download module skeleton

---
+++ src/scripts/utils/download.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /**
+   * Create a text file and prompt the user to download it.
+   * @param  {string} textContent - Text content of the file.
+   * @param  {string} mimeType - Mime type of the download.
+   */
+  createTextFileForDownload: (textContent, mimeType) => {
+    //TODO
+  }
+}
