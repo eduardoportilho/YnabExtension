59a1960941ac04fbebcbb5ca5849df189bfd4628
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 10:42:02 +0200

    CSV builder skeleton.

---
+++ src/scripts/ynab/csv-builder.js
@@ -0,0 +1,11 @@
+module.exports = {
+  /**
+   * Build a CSV string containing the given transactions.
+   * @param  {Transaction[]} transactions
+   * @return {string} CSV string
+   */
+  buildCsv: (transactions) => {
+    //TODO
+    return ''
+  }
+}
