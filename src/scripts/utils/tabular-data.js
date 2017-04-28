b138da11735e1030ac07f36722fca61e38349dcb
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 10:25:31 +0200

    Tabular data skeleton

---
+++ src/scripts/utils/tabular-data.js
@@ -0,0 +1,11 @@
+module.exports = {
+  /**
+   * Extract tabular (rows and columns) from the selection.
+   * @param  {DomSelectionRange} domSelectionRange - selection range.
+   * @return {array[]} Tabular data.
+   * @throws {Error} If no tabular data was found in the selection.
+   */
+  getTabularDataFromSelection: (domSelectionRange) => {
+    //TODO
+  }
+}
