ca3893d0c123f350a8b60646e936bafc7cafa7f4
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 08:23:38 +0200

    YNAB exporter skeleton

---
+++ src/scripts/ynab/exporter.js
@@ -0,0 +1,11 @@
+module.exports = {
+  /**
+   * Extract YNAB data from the selection and put it on a CSV string.
+   * @param  {DomSelectionRange} domSelectionRange - selection range.
+   * @return {string} CSV string with YNAB data.
+   * @throws {Error} If no YNAB data was found in the selection.
+   */
+  generateCsv: (domSelectionRange) => {
+    //TODO
+  }
+}
