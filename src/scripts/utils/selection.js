a63a222c852d62f816a9ffb9f157fa3100a141e5
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 08:16:20 +0200

    Selection module skeleton

---
+++ src/scripts/utils/selection.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {SelectionRangeDom} DOM elements of a selection range
+ * @property {Element} start - Element on the beginning of the selection.
+ * @property {Element} end - Element on the end of the selection.
+ */
+
+module.exports = {
+  /**
+   * Get the DOM elements contained in the user selection.
+   * @return {SelectionRangeDom} Selection range.
+   */
+  getSelectedElements: () => {
+    //TODO
+  }
+}
