486f7fad090cb95b7748f50e6fe21d9f9e50c0e5
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-28 10:42:33 +0200

    YNAB exporter blueprint

--- src/scripts/ynab/exporter.js
+++ src/scripts/ynab/exporter.js
@@ -1,3 +1,9 @@
+import tabular from "./utils/tabular-data"
+import columnFinder from "./ynab/column-finder"
+import transactionFactory from "./ynab/transaction-factory"
+import csvBuilder from "./ynab/csv-builder"
+
+
 module.exports = {
   /**
    * Extract YNAB data from the selection and put it on a CSV string.
@@ -6,6 +12,16 @@ module.exports = {
    * @throws {Error} If no YNAB data was found in the selection.
    */
   generateCsv: (domSelectionRange) => {
-    //TODO
+    var tabularData = tabular.getTabularDataFromSelection(domSelectionRange)
+
+    var columnInfo = columnFinder.getColumnInfo(tabularData)
+
+    var transactions = transactionFactory.createTransactions(tabularData, columnInfo)
+
+    //ynabPostExtractProcessing.processInplace(ynabTxs)
+    
+    var csv = csvBuilder.buildCsv(transactions)
+
+    return csv
   }
 }
