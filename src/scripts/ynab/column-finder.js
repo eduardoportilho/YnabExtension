58aca99ceb9a8a60e78ed82871eeef9ab25ffa0e
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-30 10:32:04 +0200

    column-finder skeleton (WIP 2)

--- src/scripts/ynab/column-finder.js
+++ src/scripts/ynab/column-finder.js
@@ -46,6 +46,44 @@ function findHeaderTypeIndexesFromTableHeader(headerLabels) {
   return columnOrder;
 };
 
+/**
+ * TODO: Refactor
+ * 
+ * Dados todos os headers de uma tabela, busca o índice do header que corresponda a um dos
+ * valores possíveis. Se houver mais de um, usa o que tiver match com maior prioridade.
+ *
+ * @param headerValues Todos os headers de uma tabela
+ * @param possibleNamesForSearchedHeader Nomes possíveis para o header desejado, em ordem
+ * decrescente de prioridade
+ * Exemplo:
+ *  headerValues = ['data', 'valor', 'saldo']
+ *  possibleNamesForSearchedHeader = ['total', 'valor', 'R$']
+ *  resultado = 1
+ */
+function findBetterMatch(headerValues, possibleNamesForSearchedHeader) {
+    var prioridadeVsIndice = {},
+        menorPrioridade = undefined;
+
+    for (var headerIdx = 0 ; headerIdx < headerValues.length ; headerIdx++) {
+        var headerVal = headerValues[headerIdx];
+        for (var prio = 0 ; prio < possibleNamesForSearchedHeader.length ; prio++) {
+            var possibleHeader = possibleNamesForSearchedHeader[prio];
+
+            if(_utils.matchIgnoringCaseAndBlank(headerVal, possibleHeader)) {
+                prioridadeVsIndice[prio] = headerIdx;
+
+                if(menorPrioridade === undefined) {
+                    menorPrioridade = prio;
+                } else {
+                    menorPrioridade = Math.min(prio, menorPrioridade);
+                }
+            }
+        }
+    }
+
+    return (menorPrioridade === undefined) ? undefined : prioridadeVsIndice[menorPrioridade];
+};
+
 module.exports = {
   getColumnInfo: getColumnInfo
 }
