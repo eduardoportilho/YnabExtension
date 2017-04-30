f29cc9e7ad052dbfe8bdd7c766f04ceb9344d29a
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-30 10:30:44 +0200

    Find header labels in tabular-data

--- src/scripts/utils/tabular-data.js
+++ src/scripts/utils/tabular-data.js
@@ -1,14 +1,25 @@
 import $ from "jquery"
 
 /**
+ * @typedef {TabularData} Table data.
+ * @property {string[][]} data - Table data.
+ * @property {string[]} [header] - Header labels.
+ */
+
+/**
  * Extract tabular (rows and columns) from the selection.
  * @param  {DomSelectionRange} domSelectionRange - selection range.
- * @return {string[][]} Tabular data.
+ * @return {TabularData} Tabular data.
  * @throws {Error} If no tabular data was found in the selection.
  */
 function getTabularDataFromSelection(domSelectionRange) {
     if (isSelectionInsideTable(domSelectionRange)) {
-      return getTabularDataFromTable(domSelectionRange)
+      var tableData = getTableDataFromSelection(domSelectionRange)
+      var headerData = getHeaderDataFromSelection(domSelectionRange)
+      return {
+        'data': tableData,
+        'header': headerData
+      }
     }
     throw Error('Could not find tabular data.')
 }
@@ -26,11 +37,11 @@ function isSelectionInsideTable(domSelectionRange) {
 }
 
 /**
- * Extract tabular (rows and columns) from a selection inside a <table>.
+ * Extract data (rows and columns) from a selection inside a <table>.
  * @param  {DomSelectionRange} domSelectionRange - selection range.
  * @return {string[][]} Tabular data.
  */
-function getTabularDataFromTable(domSelectionRange) {
+function getTableDataFromSelection(domSelectionRange) {
     var rowA = $(domSelectionRange.start).closest('tr')
     var rowB = $(domSelectionRange.end).closest('tr')
 
@@ -51,6 +62,49 @@ function getTabularDataFromTable(domSelectionRange) {
         row = row.next()
     }
     return rows
+}
+
+/**
+ * Extract data from the header of the <table>. This method consider 
+ * header the first <th> or <tr> row.
+ * @param  {DomSelectionRange} domSelectionRange - selection range.
+ * @return {string[][]|undefined} Header data or undefined, if not found.
+ */
+function getHeaderDataFromSelection(domSelectionRange) {
+    var table = $(domSelectionRange.start).closest('table')
+    if (table.length === 0) {
+      table = $(domSelectionRange.end).closest('table')
+    }
+    if (table.length === 0) {
+      return undefined
+    }
+
+    // 1st try: first <th> row
+    var th = table.find('th')
+    if (th.length > 0) {
+      // only 1st th row
+      var headerCols = th.first().parent().find('th')
+      var headerVals = headerCols.map(function(i, el){
+          return jQuery(el).text()
+      }).get()
+      return headerVals
+    }
+
+    //2nd try: first <tr> row
+    var firstRow = table.find('tr:first')
+    if (firstRow.length > 0) {
+      var headerCols = firstRow
+        .find('td')
+        .filter(function(i,td) {
+          //ignore td with children
+          return jQuery(td).children().length === 0
+        })
+      var headerVals = headerCols.map(function (i, el) {
+          return jQuery(el).text()
+      }).get()
+      return headerVals
+    }
+    return undefined
 }
 
 module.exports = {
