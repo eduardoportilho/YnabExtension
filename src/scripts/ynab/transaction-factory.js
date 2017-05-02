09fac472b95674e1ed33eb4b21ac08f20315c0ac
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-02 08:12:01 +0200

    Transaction factory implementation (WIP 2)

--- src/scripts/ynab/transaction-factory.js
+++ src/scripts/ynab/transaction-factory.js
@@ -6,31 +6,50 @@
  * @property {string} outflow - Outflow with 2 decimal places (could be negative).
  */
 
-module.exports = {
+import {date, num} from 'jsturbo'
+
   /**
    * Build YNAB transactions from the tabular data based on the column info.
    * @param  {TabularData} tabularData - Tabular data.
    * @param  {ColumnInfo} columnInfo - Column information.
    * @return {Transaction[]} YNAB transactions.
    */
-  createTransactions: (tabularData, columnInfo) => {
-    var ynabTxs = []
-    for(var row = 0; row < tabularData.data.length; row++) {
-      var rowValues = tabularData.data[row]
-      try {
-        var ynabTx = {}
-        //TODO formatDate(date); //Date in DD/MM/YYYY format
-        ynabTx.date = rowValues[columnInfo.dateIndex]
-        //TODO clearCsvString(payee);
-        ynabTx.payee = columnInfo.payeeIndex >= 0 ? rowValues[columnInfo.payeeIndex] : ''
-        // TODO formatMoney(inflow);
-        ynabTx.inflow = columnInfo.inflowIndex >= 0 ? rowValues[columnInfo.inflowIndex] : ''
-        ynabTx.outflow = '' // Ignore outflow, using negative inflow instead
-        ynabTxs.push(ynabTx)
-      } catch (any) {
-        console.log("Ignoring invalid row: (" + rowValues + ")(" + any.message + ")")
-      }
+}
+function createTransactions(tabularData, columnInfo) {
+  var ynabTxs = []
+  for(var row = 0; row < tabularData.data.length; row++) {
+    var rowValues = tabularData.data[row]
+    try {
+      var ynabTx = {}
+      ynabTx.date = getDate(rowValues, columnInfo)
+      ynabTx.payee = getPayee(rowValues, columnInfo)
+      ynabTx.inflow = getInflow(rowValues, columnInfo)
+      ynabTx.outflow = '' // Ignore outflow, using negative inflow instead
+      ynabTxs.push(ynabTx)
+    } catch (any) {
+      console.log("Ignoring invalid row: (" + rowValues + ")(" + any.message + ")")
     }
-    return ynabTxs
   }
+  return ynabTxs
+}
+
+function getDate (rowValues, columnInfo) {
+  var txDate = date.fromString(rowValues[columnInfo.dateIndex])
+  return date.toStringDMY(txDate)
+}
+
+function getPayee (rowValues, columnInfo) {
+  return columnInfo.payeeIndex >= 0 ? rowValues[columnInfo.payeeIndex] : ''
+}
+
+function getInflow (rowValues, columnInfo) {
+  var inflow = 0
+  if (columnInfo.inflowIndex >= 0) {
+    inflow = num.toNumber(rowValues[columnInfo.inflowIndex])
+  }
+  return num.format(inflow, {'decimalPlaces': 2})
+}
+
+module.exports = {
+  createTransactions: createTransactions
 }
