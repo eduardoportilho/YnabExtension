ab3c45fb4e841a4e8fa0646068dab1b08b8b72d8
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-12 18:10:20 +0200

    download.js implementation

--- src/scripts/utils/download.js
+++ src/scripts/utils/download.js
@@ -1,10 +1,13 @@
 module.exports = {
   /**
    * Create a text file and prompt the user to download it.
+   * @param  {Window} aWindow - Window object.
    * @param  {string} textContent - Text content of the file.
-   * @param  {string} mimeType - Mime type of the download.
+   * @param  {string} [mimeType='text/plain'] - Mime type of the download.
    */
-  createTextFileForDownload: (textContent, mimeType) => {
-    //TODO
+  createTextFileForDownload: (aWindow, textContent, mimeType) => {
+    mimeType = mimeType || 'text/plain'
+    var encodedUri = encodeURI(`data:${mimeType};charset=utf-8,${textContent}`)
+    aWindow.open(encodedUri);
   }
 }
