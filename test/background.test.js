a2f2345fe1d0d3513363baee69778d05c65b9029
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-14 12:51:44 +0200

    100% coverage on background.js

--- test/background.test.js
+++ test/background.test.js
@@ -3,43 +3,53 @@ import proxyquire from 'proxyquire'
 import td from 'testdouble'
 
 describe("background", () => {
-  describe("ynabExportSelection", () => {
-    it("should create context menu", () => {
-      // given
-      var contextMenus_create = td.function()
-      global.chrome = {
-        contextMenus: { create: contextMenus_create }
+  var contextMenus_create
+  var notifications_create
+  var runtime_getURL
+  var runtime_onMessage_addListener
+  var tab_sendMessage
+  var background
+
+  beforeEach(() => {
+    contextMenus_create = td.function()
+    notifications_create = td.function()
+    runtime_getURL = td.function()
+    runtime_onMessage_addListener = td.function()
+    tab_sendMessage = td.function()
+    background = proxyquire('../src/scripts/background', {
+      './utils/ext': {
+        contextMenus: { create: contextMenus_create },
+        notifications: { create: notifications_create },
+        tabs: { sendMessage: tab_sendMessage },
+        runtime: {
+          onMessage: { addListener: runtime_onMessage_addListener },
+          getURL: runtime_getURL
+        }
       }
+    })
+  })
 
-      // when
-      var background = proxyquire('../src/scripts/background', {})
+  afterEach(() => {
+    td.reset()
+  })
 
-      // then
+
+  describe("component initialization", () => {
+    it("should create context menu and add listener", () => {
+      // when: the component is initialized, then:
+      td.verify(contextMenus_create({
+        "title": "Export to YNAB",
+        "contexts":["selection"],
+        "onclick": background.ynabExportSelection
+      }))
       td.verify(
-        contextMenus_create({
-          "title": "Export to YNAB",
-          "contexts":["selection"],
-          "onclick": background.ynabExportSelection
-        })
+        runtime_onMessage_addListener(background.onRuntimeMessage)
       )
     })
+  })
 
+  describe("ynabExportSelection", () => {
     it("should send message to the clicked tab", () => {
-      // given
-      var tab_sendMessage = td.function()
-      global.chrome = {
-        contextMenus: {
-          create() {}
-        }
-      }
-      var background = proxyquire('../src/scripts/background', {
-        './utils/ext': {
-          tabs: {
-            sendMessage: tab_sendMessage
-          }
-        }
-      })
-      
       // when
       background.ynabExportSelection(
         {selectionText: 'testSelectionText'},
@@ -56,4 +66,40 @@ describe("background", () => {
       )
     })
   })
+
+  describe("onRuntimeMessage", () => {
+    it("should handle 'displayMessage' action", () => {
+      // given
+      td.when(runtime_getURL('icons/icon-48.png')).thenReturn('test-icon')
+
+      // when
+      background.onRuntimeMessage({
+        action: 'displayMessage',
+        data: {
+          title: 'test-title',
+          message: 'test-message'
+        }
+      })
+
+      // then
+      td.verify(
+        notifications_create('ynab.export.error', {
+          type: 'basic',
+          title: 'test-title',
+          message: 'test-message',
+          iconUrl: 'test-icon'
+        })
+      )
+    })
+    it("should ignore other actions", () => {
+      // when
+      background.onRuntimeMessage({action: 'otherMessage'})
+
+      // then
+      td.verify(notifications_create(), {times: 0, ignoreExtraArgs: true})
+      td.verify(runtime_getURL(), {times: 0, ignoreExtraArgs: true})
+      td.verify(tab_sendMessage(), {times: 0, ignoreExtraArgs: true})
+    })
+  })
+
 })
