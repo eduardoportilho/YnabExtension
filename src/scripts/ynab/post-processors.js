dcb4d0e311b55cdf1a32490631e687ad7619f919
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-05-21 10:30:44 +0200

    Refactor post-processor to extract invert-credit-card

--- src/scripts/ynab/post-processors.js
+++ src/scripts/ynab/post-processors.js
@@ -1,54 +1,13 @@
 import transactionStats from './transaction-stats'
-import {obj} from 'jsturbo'
+import invertCreditCard from './processors/invert-credit-card.js'
 
 /**
  * List of post-processors
  * @type {Object}
  */
-const POST_PROCESSORS = {
-  /**
-   * If more than X% of the transactions are inflows, probably they are from a credit card account.
-   * In this case we want to invert the signal of the inflow.
-   * @param {YnabTx[]} transactions - Transactions before processing.
-   * @param {YnabTxStats} stats     - Transactions statistics.
-   * @return {YnabTx[]} Processed YNAB transaction
-   */
-  'invert inflow signal of credit card transactions': (transactions, stats) => {
-    if (stats.inflowPercentage > 0.6) {
-      return transactions.map(invertTxAmountSignal)
-    } else {
-      return transactions 
-    }
-  }
-}
-
-/**
- * Invert the signal of a transaction amount
- * @param  {YnabTx} transaction - Transaction.
- * @return {YnabTx} Transaction with the inverted amount.
- */
-function invertTxAmountSignal(ynabTx) {
-  if (!obj.isEmpty(ynabTx.inflow)) {
-    ynabTx.inflow = invertNumberStringSignal(ynabTx.inflow)
-  }
-  else if (!obj.isEmpty(ynabTx.outflow)) {
-    ynabTx.outflow = invertNumberStringSignal(ynabTx.outflow)
-  }
-  return ynabTx
-}
-
-/**
- * Invert the signal of the number in the provided string
- * @param  {string} numberString - Number as string
- * @return {string} Number with the inverted signal as string
- */
-function invertNumberStringSignal(numberString) {
-  if (numberString.charAt(0) === '-') {
-    return numberString.replace(/\-/g, '')
-  } else {
-    return '-' + numberString
-  }
-}
+const POST_PROCESSORS = [
+  invertCreditCard
+]
 
 /**
  * Execute all post-processors on the original transactions.
@@ -58,8 +17,11 @@ function invertNumberStringSignal(numberString) {
 function processTransactions(transactions) {
   let stats = transactionStats.generateStatistics(transactions)
   var processedTransactions = transactions
-  for (let key in POST_PROCESSORS) {
-    processedTransactions = POST_PROCESSORS[key](processedTransactions, stats)
+  for (var i = 0 ; i < POST_PROCESSORS.length ; i++) {
+    var processor = POST_PROCESSORS[i]
+    if (processor.shouldApply(processedTransactions, stats)) {
+      processedTransactions = processTransactions(processedTransactions, stats)
+    }
   }
   return processedTransactions
 }
---
+++ src/scripts/ynab/processors/invert-credit-card.js
@@ -0,0 +1,63 @@
+/**
+ * Invert credit card transactions:
+ * If most of the transactions are positive, they are likely to belong to a credit card statement.
+ * Credit card transactions are outflows, so the signal should be negative.
+ */
+
+import {obj} from 'jsturbo'
+
+/**
+ * Check if the processor should be applyed.
+ * @param {YnabTx[]} transactions - Transactions before processing.
+ * @param {YnabTxStats} stats     - Transactions statistics.
+ * @return {Boolean}
+ */
+function shouldApply(transactions, stats) {
+  return stats.inflowPercentage > 0.6
+}
+
+/**
+ * Apply the processor.
+ * @param {YnabTx[]} transactions - Transactions before processing.
+ * @param {YnabTxStats} stats     - Transactions statistics.
+ * @return {YnabTx[]} Processed YNAB transaction
+ */
+function processTransactions(transactions, stats) {
+  return transactions.map(invertTxAmountSignal)
+}
+
+/**
+ * Invert the signal of a transaction amount
+ * @param  {YnabTx} transaction - Transaction.
+ * @return {YnabTx} Transaction with the inverted amount.
+ */
+function invertTxAmountSignal(ynabTx) {
+  if (!obj.isEmpty(ynabTx.inflow)) {
+    ynabTx.inflow = invertNumberStringSignal(ynabTx.inflow)
+  }
+  else if (!obj.isEmpty(ynabTx.outflow)) {
+    ynabTx.outflow = invertNumberStringSignal(ynabTx.outflow)
+  }
+  return ynabTx
+}
+
+/**
+ * Invert the signal of the number in the provided string
+ * @param  {string} numberString - Number as string
+ * @return {string} Number with the inverted signal as string
+ */
+function invertNumberStringSignal(numberString) {
+  if (numberString.charAt(0) === '-') {
+    return numberString.replace(/\-/g, '')
+  } else {
+    return '-' + numberString
+  }
+}
+
+module.exports = {
+  shouldApply: shouldApply,
+  processTransactions: processTransactions,
+  //private methods exposed for testing only
+  invertTxAmountSignal: invertTxAmountSignal,
+  invertNumberStringSignal: invertNumberStringSignal
+}
