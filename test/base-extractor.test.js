0cd37d8b8d6e2e0a567d5402ea0c224a927211f0
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-06-04 09:59:50 +0200

    Add tests to base-extractor

--- test/base-extractor.test.js
+++ test/base-extractor.test.js
@@ -4,11 +4,17 @@ import td from 'testdouble'
 
 describe('base-extractor', function() {
   let baseExtractor
+  let tableExtractor
+  let domExtractor
 
   beforeEach(() => {
+    // This is necessary to avoid caching './table-extractor' and './dom-extractor', which would break the integration tests
+    proxyquire.noPreserveCache()
+    tableExtractor = td.object(['getTabularDataFromSelection'])
+    domExtractor = td.object(['getTabularDataFromSelection'])
     baseExtractor = proxyquire('../src/scripts/utils/data_extractors/base-extractor', {
-      './table-extractor': td.object(),
-      './dom-extractor': td.object(['getSelectedElements'])
+      './table-extractor': tableExtractor,
+      './dom-extractor': domExtractor
     })
   })
 
@@ -16,20 +22,64 @@ describe('base-extractor', function() {
     td.reset()
   })
 
+  describe('getTabularDataFromSelection', function() {
+    it('should use tabular extractor if possible', function() {
+      // // given
+      let tableData = { data: [['table-data']] }
+      td.when(tableExtractor.getTabularDataFromSelection(td.matchers.anything()))
+        .thenReturn(tableData)
+
+      // when
+      let data = baseExtractor.getTabularDataFromSelection('test-selection', 'test-url')
+
+      // then
+      expect(data).to.deep.equal(tableData)
+    })
+
+    it('should use DOM extractor if table extractor throws', function() {
+      // given 
+      let domData = { data: [['dom-data']] }
+      td.when(tableExtractor.getTabularDataFromSelection(td.matchers.anything()))
+        .thenThrow(new Error('table extractor throws'))
+
+      td.when(domExtractor.getTabularDataFromSelection(td.matchers.anything()))
+        .thenReturn(domData)
+
+      // when
+      let data = baseExtractor.getTabularDataFromSelection('test-selection', 'test-url')
+
+      // then
+      expect(data).to.deep.equal(domData)
+    })
+
+    it('should throw if both table and dom extractors thow', function() {
+      // given 
+      td.when(tableExtractor.getTabularDataFromSelection(td.matchers.anything()))
+        .thenThrow(new Error('table extractor throws'))
+      td.when(domExtractor.getTabularDataFromSelection(td.matchers.anything()))
+        .thenThrow(new Error('DOM extractor throws'))
+
+      // then
+      expect (() => {
+        baseExtractor.getTabularDataFromSelection('test-selection', 'test-url')
+      }).to.throw('Could not find tabular data')
+    })
+  })
+
   describe('normalizeTabularData', function() {
     it('should normalize tabular data with no empty cells', function() {
       let data = {
         data:  [
-          ['A', 'B', 'C', 'D'],
           ['A', 'B', 'C'],
+          ['A', 'B', 'C', 'D'],
           ['A', 'B']
         ]
       }
       expect(baseExtractor._normalizeTabularData(data))
         .to.deep.equal({
           data:  [
-            ['A', 'B', 'C', 'D'],
             ['A', 'B', 'C', ''],
+            ['A', 'B', 'C', 'D'],
             ['A', 'B', '', '']
           ]})
     })
